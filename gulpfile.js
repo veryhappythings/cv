0577499c4632a0671f8344c3a939f14762b1dd19
Mac Chapman <mac@veryhappythings.co.uk>
2020-05-06 15:18:35 +0100

    Update all dependencies
    It's been a long time since I did this. Had to rewrite the gulpfile.
    It's much tidier now though!

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,54 @@
-var gulp = require('gulp');
+const { dest, src, series, watch } = require('gulp');
 var path = require('path');
 var fs = require('fs');
 
 var markdown = require('gulp-markdown');
 var less = require('gulp-less');
 var wrapper = require('gulp-wrapper');
-var minifyCSS = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 
 var browserSync = require('browser-sync');
 
 var paths = {
+  dist: 'dist',
   less: './less/**/*.less',
-  dist: 'dist'
+  md: ['*.md', '*.html'],
 }
 
-gulp.task('less', function () {
-  return gulp.src(paths.less)
+function buildLess(cb) {
+  src(paths.less)
     .pipe(less({
       paths: [path.join(__dirname, 'less', 'includes')]
     }))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('./dist/css'));
-});
+    .pipe(cleanCSS())
+    .pipe(dest('./dist/css'));
+  cb();
+};
 
-gulp.task('less-watch', ['less'], browserSync.reload);
-
-gulp.task('md', function () {
-  return gulp.src('index.md')
+function buildMd(cb) {
+  src('index.md')
     .pipe(markdown())
     .pipe(wrapper({
       header: fs.readFileSync('header.html', 'utf8'),
       footer: fs.readFileSync('footer.html', 'utf8')
     }))
-    .pipe(gulp.dest(paths.dist));
-});
+    .pipe(dest(paths.dist));
 
-gulp.task('md-watch', ['md'], browserSync.reload);
+  cb();
+};
 
-gulp.task('serve', ['md', 'less'], function() {
-  browserSync({
+function serve(cb) {
+  browserSync.init({
+    files: paths.dist,
     server: {
       baseDir: paths.dist
     }
   });
-  gulp.watch(paths.less, ['less-watch']);
-  gulp.watch(['*.md', '*.html'], ['md-watch']);
-});
 
-gulp.task('default', ['serve']);
+  watch(paths.less, buildLess);
+  watch(paths.md, buildMd);
+
+  cb();
+};
+
+exports.default = series(buildMd, buildLess, serve);
